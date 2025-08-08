357c2014f4f3910c31cbce9b83d296af4e275a79
Aaron <aarondsimpson@outlook.com>
2025-08-07 22:44:25 -0400

    added prompt user for court type in dialogs to bring box to front

--- src/dialogs/player_dialogs.py
+++ src/dialogs/player_dialogs.py
@@ -1,5 +1,19 @@
 from tkinter import simpledialog, messagebox
 
+def prompt_user_for_court_type(self):
+    self.lift()
+    self.attributes('-topmost', True)
+    self.after(100, lambda: self.attributes('-topmost', False))
+
+    value = simpledialog.askstring(
+        "Select Court Type",
+        "Enter 'Half' or 'Full':",
+        parent=self,
+        initialvalue='Half',
+    )   
+    return value if value in ['Half', 'Full'] else "Half"
+
+
 def prompt_add_player(parent):
     return simpledialog.askstring("Add Player", "Enter Player Name:", parent=parent)
 
